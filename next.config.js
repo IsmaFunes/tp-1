9c3774d9fb2a9ab6fd6a5e83ace7e996df8e4d22
IsmaFunes <ismafunes66@gmail.com>
2022-03-27 22:14:32 -0300

    fix deploy

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: isProd ? '/tp-1' : '',
-  assetPrefix: isProd ? '/tp-1' : ''
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || ''
 }
 
 module.exports = nextConfig
