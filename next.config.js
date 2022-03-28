edbb482bd81be876bc4cba951969c4c3f284f44c
IsmaFunes <ismafunes66@gmail.com>
2022-03-27 22:05:34 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/tp-1' : '',
+  assetPrefix: isProd ? '/tp-1' : ''
 }
 
 module.exports = nextConfig
