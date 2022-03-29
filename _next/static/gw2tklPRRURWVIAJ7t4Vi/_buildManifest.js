96df0cc7495f572c1bca000c87f22e5a13184537
IsmaFunes <33071292+IsmaFunes@users.noreply.github.com>
2022-03-29 14:21:52 +0000

    Deploying to gh-pages from @ IsmaFunes/tp-1@2c69d11479483ac0d3cf3dff916a9b13cdfe7769 🚀

--- _next/static/gw2tklPRRURWVIAJ7t4Vi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/36bcf0ca-09842b14c75e2da0.js","static/chunks/571-8a2e26e298c818b8.js","static/chunks/pages/index-698956f464e063b2.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
